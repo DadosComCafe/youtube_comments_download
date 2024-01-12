3366969242cba59a079f17ca53c0c032e090acd8
DadosComCafe <dadoscomcafe.dev@gmail.com>
2024-01-12 12:26:25 -0300

    (get_videos/main.py): Create a function to export the json in parquet file

--- get_videos/main.py
+++ get_videos/main.py
@@ -7,6 +7,7 @@ from utils import chunkify
 
 logging.basicConfig(level=logging.INFO)
 
+
 def get_video_content(video_id: str, api_key: str) -> list | str:
     page_token = ""
     target_url = f"https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&part=replies&pageToken={page_token}&videoId={video_id}&key={api_key}&alt=json"
@@ -17,7 +18,7 @@ def get_video_content(video_id: str, api_key: str) -> list | str:
         if "nextPageToken" not in json_content:
             items_content = json_content["items"]
             return items_content, items_content[0]["snippet"]["videoId"]
-        
+
         next_page_token = json_content.get("nextPageToken")
         items_content = []
 
@@ -33,8 +34,9 @@ def get_video_content(video_id: str, api_key: str) -> list | str:
 
     return "Video fora do ar!"
 
+
 def send_json_to_mongo(dict_credentials: dict, items_content: json, video_id: str):
-    connection_config = f"mongodb://{dict_credentials["mongo_user"]}:{dict_credentials["mongo_password"]}@{dict_credentials["mongo_host"]}:{dict_credentials["mongo_port"]}/"
+    connection_config = f"""mongodb://{dict_credentials["mongo_user"]}:{dict_credentials["mongo_password"]}@{dict_credentials["mongo_host"]}:{dict_credentials["mongo_port"]}/"""
     client = MongoClient(connection_config)
     collection = client["youtube_comments"][video_id]
     try:
@@ -43,30 +45,49 @@ def send_json_to_mongo(dict_credentials: dict, items_content: json, video_id: st
             id_comment = item["snippet"]["topLevelComment"]["id"]
             filter_comment_id = {"commentId": id_comment}
 
-            operations.append(
-                UpdateOne(filter_comment_id, {"$set": item}, upsert=True)
-            )
+            operations.append(UpdateOne(filter_comment_id, {"$set": item}, upsert=True))
         for index, chunk in enumerate(chunkify(operations, 100)):
             n_ops = ceil(len(operations) / 100)
-            logging.info(f"Executing inserting in collection, chunk {index + 1}/{n_ops}")
+            logging.info(
+                f"Executing inserting in collection, chunk {index + 1}/{n_ops}"
+            )
             result = collection.bulk_write(chunk, ordered=False)
             logging.info(f"{result.bulk_api_result}\n")
-        logging.info(f"The json content has been inserted successfully for the video_id: {video_id}")
+        logging.info(
+            f"The json content has been inserted successfully for the video_id: {video_id}"
+        )
     except Exception as e:
         raise Exception(f"An error: {e}")
 
 
+def export_json_parquet(items_content: json, video_id: str):
+    from pandas import DataFrame
+    from pyarrow import parquet, Table
+
+    temp_df = DataFrame(items_content)
+    table = Table.from_pandas(temp_df)
+    file_path = f"videos_parquet/{video_id}.parquet"
+    try:
+        parquet.write_table(table, file_path)
+        logging.info(f"The parquet file to {video_id} has been exported successfully!")
+    except Exception as e:
+        logging.error(f"An error {e}")
 
 
 if __name__ == "__main__":
     from decouple import config
+
     my_api_key = config("API_KEY")
     dict_credentials = {
         "mongo_user": config("MONGO_USERNAME"),
         "mongo_password": config("MONGO_PASSWORD"),
         "mongo_host": config("MONGO_HOST"),
-        "mongo_port": config("MONGO_PORT")
+        "mongo_port": config("MONGO_PORT"),
     }
     video_id = "PzUmRTcozms"
     video_content = get_video_content(video_id=video_id, api_key=my_api_key)
-    send_json_to_mongo(dict_credentials=dict_credentials, items_content=video_content, video_id=video_id)
+    send_json_to_mongo(
+        dict_credentials=dict_credentials,
+        items_content=video_content,
+        video_id=video_id,
+    )

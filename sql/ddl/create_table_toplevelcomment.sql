6286d8182edd421fe39bddb5980cad8f018d6595
DadosComCafe <dadoscomcafe.dev@gmail.com>
2024-01-17 16:12:02 -0300

    (send_comment_to_postgres/main.py): Create function and process to insert toplevelcomment to postgres

--- sql/ddl/create_table_toplevelcomment.sql
+++ sql/ddl/create_table_toplevelcomment.sql
@@ -1,6 +1,6 @@
 create table if not exists comment(
     id VARCHAR(30) PRIMARY KEY,
-    author_name VARCHAR(20),
+    author_name VARCHAR(100),
     text_original TEXT,
     published_date TEXT
 );
